7d0cebb2d505fdd9364941ac21fd5895dd84d1b8
Edison Yang <edison.cy.yang@gmail.com>
2016-06-30 23:57:36 -0700

    Final Solution
     First load all the users that we need to check into a list, then go
    through the list to check the courses taken by these users.

--- src/main/User.java
+++ src/main/User.java
@@ -38,7 +38,19 @@ public class User {
 		}
 		
 		List<User> friends = user.getDirectFriendsForUser();
-		for(User f : friends) { // Go through the friend list
+		List<User> allFriends = new ArrayList<User>();
+		
+		// Load friends of original user, and friends of friends into one big list	
+		for(User u: friends) {
+			List<User> ff = u.getDirectFriendsForUser();
+			for(User f_f: ff) {
+				allFriends.add(f_f);
+			}
+			allFriends.add(u);
+		}
+		
+		// Go through the friend list
+		for(User f : allFriends) { 
 			String name = f.name;
 			if(!hashU.contains(name)) { // only go through the course list if this friend has not been checked
 				hashU.add(name); // add friends visited to hashU if not already existed
@@ -56,27 +68,6 @@ public class User {
 				}
 			}
 			
-			// Now this part will work on friends of friend
-			List<User> f_friends = f.getDirectFriendsForUser();
-			for(User ff: f_friends) {
-				String fName = ff.name;
-				if(!hashU.contains(fName)) {
-					hashU.add(fName);
-					
-					List<String> coursesOfFF = ff.getAttendedCoursesForUser();
-					for(String fc: coursesOfFF) {
-						if(!hashUC.contains(fc)) {
-							if(!hashC.containsKey(fc)) {
-								hashC.put(fc, 1);
-							}
-							else {
-								hashC.put(fc, hashC.get(fc) + 1);
-							}
-							
-						}
-					}
-				}
-			}
 		}
 		
 		Set set = hashC.entrySet();
